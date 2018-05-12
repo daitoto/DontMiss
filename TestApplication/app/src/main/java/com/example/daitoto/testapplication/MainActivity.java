42cfaa05e77e606652b54afe48898a99690c3798
DaiToto <1500012781@pku.edu.cn>
2018-05-12 17:14:12 +0800

    2

--- TestApplication/app/src/main/java/com/example/daitoto/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/daitoto/testapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.daitoto.testapplication;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -18,12 +19,58 @@ import android.widget.Button;
 import android.app.Fragment;
 import android.view.WindowManager;
 import android.view.Gravity;
+import android.widget.ImageView;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.os.Environment;
+import android.net.Uri;
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ViewPager mViewPager;
+    private ImageView mImageView;
+    private ImageView iv_CameraImg;
+    private static final int REQUEST_IMAGE_CAPTURE_THUMB = 1;
+    private static final int REQUEST_IMAGE_CAPTURE_FULL = 2;
+
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+    private static final String CAMERA_DIR = "/dcim/";
+    private static final String albumName ="CameraSample";
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -31,25 +78,41 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_calender:
+                    mTextMessage.setText(R.string.title_calender);
+                    button_calender_click();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_society:
+                    mTextMessage.setText(R.string.title_society);
+                    button_society_click();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_user:
+                    mTextMessage.setText(R.string.title_user);
+                    button_user_click();
                     return true;
             }
             return false;
         }
     };
 
+    public void button_calender_click(){
+        //TODO
+    }
+
+    public void button_society_click(){
+        //TODO
+    }
+
+    public void button_user_click(){
+        //TODO
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        iv_CameraImg = (ImageView) findViewById(R.id.camera);
+        verifyStoragePermissions(MainActivity.this);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -80,7 +143,6 @@ public class MainActivity extends AppCompatActivity {
 
     private  void add_photos()
     {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         final Dialog mdialog = new Dialog(MainActivity.this, R.style.photo_dialog);
         mdialog.setContentView(View.inflate(MainActivity.this, R.layout.popup_window, null));
         // 弹出对话框
@@ -108,15 +170,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         camera.setOnClickListener(new View.OnClickListener() {
+
+            Intent intent = null;
             @Override
             public void onClick(View v) {
-//                takePhoto();
                 mdialog.dismiss();
 //                Toast.makeText(MainActivity.this, "Opening Camera",Toast.LENGTH_SHORT).show();
-
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                }
+                catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                Uri uri = Uri.fromFile(photoFile);
+                intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(intent, 0);
             }
+
         });
         mdialog.show();
+
+    }
+
+    String mPublicPhotoPath;
+    private File createImageFile() throws IOException {
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM);
+        Log.i("toto", "path:" + path.getAbsolutePath());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        Log.i("toto", path + "/" + imageFileName + ".jpg");
+        File image = File.createTempFile(imageFileName,".jpg", path);
+        mPublicPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        Log.v("toto", " res:" + resultCode);
+        if(resultCode != Activity.RESULT_OK) return;
+        File file = new File(mPublicPhotoPath);
+        Uri uri = Uri.fromFile(file);
+        Log.i("toto", uri.getEncodedPath());
+        iv_CameraImg.setImageURI(uri);
 
     }
 
--- TestApplication/app/src/main/java/com/example/daitoto/testapplication/submitActivity.java
+++ TestApplication/app/src/main/java/com/example/daitoto/testapplication/submitActivity.java
@@ -1,5 +1,13 @@
 package com.example.daitoto.testapplication;
 
-public class submitActivity {
-    
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
 }
